7a933c40dba72ecff0234d6691982e4ebbc9605f
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-11-06 15:16:02 +0530

    functionality completed

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,19 +4,7 @@ import { Context } from "../Context";
 export default function Form(){
     const {todos,add} = useContext(Context)
     const [value,setValue] = useState("")
-    // const [todo,setTodo] = useState([
-    //     {
-    //         text: "This is a sample todo",
-    //         id: nanoid(),
-    //         isDone: false
-    //     }
-    // ])
-    // const addTodo = text => {
-    //     const newTodos = [...todo,{text,id: nanoid(),isDone: false}]
-    //     setTodo(newTodos)
-    // }
-    // console.log(todo)
-    
+
     const handleSubmit = event => {
         event.preventDefault()
         if(value){
@@ -27,11 +15,6 @@ export default function Form(){
         
     }
     console.log(todos)
-    // const list = todo.map(e => (
-    //     <li key={e.id}>
-    //         {e.text}
-    //     </li>
-    // )) 
     return(
         <>
         <form onSubmit={handleSubmit}>
@@ -41,7 +24,7 @@ export default function Form(){
                 onChange={e=>setValue(e.target.value)}
                 placeholder="Type your todo"
             />
-            <button><i class="ri-add-line ri-1.3x"></i></button>
+            <button>+</button>
         </form>
         </>
     )
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,16 +1,23 @@
-import React,{useContext} from "react";
+import React,{useContext,useState} from "react";
 import { Context } from "../Context";
 
 export default function Todo({text,id}){
     const {deleteTodo,toggleMarkDone} = useContext(Context)
+    const [hover,setHover] = useState(false)
+    const deleteIcon = hover ? "ri-delete-bin-fill" : "ri-delete-bin-line"
+    const checkIcon = hover ? "ri-check-double-line" : "ri-check-line"
 
     return(
         <>
-         <li key={id}>
+         <li 
+            onMouseEnter={()=>setHover(true)} 
+            onMouseLeave={()=>setHover(false)}
+            key={id}
+        >
             {text}
             <div>
-            <i class="ri-delete-bin-line" onClick={()=>deleteTodo(id)}></i>
-            <i class="ri-check-line" onClick={()=>toggleMarkDone(id)}></i>
+            <i class={deleteIcon} onClick={()=>deleteTodo(id)}></i>
+            <i class={checkIcon} onClick={()=>toggleMarkDone(id)}></i>
             </div>
          </li>
         </>
